0da233004fec9efba248f7bccd1e8b7cf4872a16
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-02-04 09:53:23 -0700

    Add index.css, update app.js/index.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>
-            Hello World.
-        </div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     );
 };
 

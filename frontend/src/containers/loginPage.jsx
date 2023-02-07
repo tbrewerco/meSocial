d6fbd1f4ce1e80150a017dbbc974875fb116f5ae
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-02-07 15:59:57 -0700

    WIP: Add header to login page

--- frontend/src/containers/loginPage.jsx
+++ frontend/src/containers/loginPage.jsx
@@ -3,6 +3,7 @@ import danceVid from '../assets/dance_1.mp4';
 import logo from '../assets/me_share5.png';
 import LoginButton from '../components/loginButton';
 import LogoutButton from '../components/logoutButton';
+import Header from '../components/Header';
 
 const LoginPage = () => {
 
@@ -19,7 +20,12 @@ const LoginPage = () => {
                     autoPlay
                     className='w-full h-full object-cover'
                 />
-
+                <Header
+                    heading='Login to your account'
+                    paragraph="Don't have an account yet?"
+                    linkName='Signup'
+                    linkUrl='/signup'
+                />
                 <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
                     <div>
                         <div className='p-5'>

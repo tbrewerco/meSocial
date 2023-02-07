fed6fca26c8e19ce0c890a7aa64c17a94604e1fa
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-02-06 17:37:29 -0700

    WIP: Add google login

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -6,6 +6,11 @@ import danceVid from '../assets/dance_1.mp4';
 import logo from '../assets/me_share5.png';
 
 const Login = () => {
+
+    const responseGoogle = (response) => {
+        console.log(response);
+    }
+
     return (
         <div className='flex justify-start items-center flex-col h-screen'>
             <div className='relative w-full h-full'>
@@ -13,7 +18,7 @@ const Login = () => {
                 <video
                     src={danceVid}
                     type="video/mp4"
-                    Loop
+                    loop
                     controls={false}
                     muted
                     autoPlay
@@ -21,8 +26,29 @@ const Login = () => {
                 />
 
                 <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
-                    <div className='p-5'>
-                        <img src={logo} width='250px' alt='logo'></img>
+                    <div>
+                        <div className='p-5'>
+                            <img src={logo} width='300px' alt='logo' />
+                        </div>
+                        <div className='shadow-2xl'>
+                            <GoogleLogin
+                                clientId={import.meta.env.REACT_APP_GOOGLE_CLIENT_ID}
+
+                                render={(renderProps) => (
+                                    <button
+                                        type='button'
+                                        className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                                        onClick={renderProps.onClick}
+                                        disabled={renderProps.disabled}
+                                    >
+                                        <FcGoogle className='mr-2' /> Sign in with Google
+                                    </button>
+                                )}
+                                onSuccess={responseGoogle}
+                                onFailure={responseGoogle}
+                                cookiePolicy='single_host_origin'
+                            />
+                        </div>
                     </div>
                 </div>
 

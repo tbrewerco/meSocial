a21e08503c837e7f06b48e3ff8ebc0b622547bcb
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-02-10 15:09:24 -0700

    Bugfix: typo

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const fixedInputClassName = 'rounded-md appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-purple-500 focus:border-purple-500 focus:z-10 sm:text-sm';
+const fixedInputClassName = 'rounded-md appearance-none block w-full px-2 py-2 border border-gray-300 border hover:border-red-300 placeholder-gray-500 text-gray-500 focus:outline-none focus:ring-red-500 text-sm focus:border-red-500 focus:z-10 sm:text-sm';
 
 const Input = (
     handleChange,
@@ -11,7 +11,7 @@ const Input = (
     name,
     type,
     isRequired = false,
-    placeHolder,
+    placeholder,
     customClass
 ) => {
     return (
@@ -27,6 +27,7 @@ const Input = (
                 type={type}
                 required={isRequired}
                 className={fixedInputClassName + customClass}
+                placeholder={placeholder}
             />
         </div>
     )

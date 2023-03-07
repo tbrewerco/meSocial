0265991287c292f82b7998122c2155d388ed53fa
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-03-06 17:00:29 -0700

    Adds remember me box and forgotten password link

--- frontend/src/components/FormExtra.jsx
+++ frontend/src/components/FormExtra.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
 
-const FormExtra = () => {
-    return (
-        <div className='flex items-center justify-between'>
-            <div className='flex items-center'>
+const FormExtra = (props) => {
+    if (props.display)
+        return (
+            <div className='flex items-center justify-between'>
+                <div className='flex items-center'>
 
-                <input
-                    type='checkbox'
-                    id='remember-me'
-                    name='remember-me'
-                    className='w-4 h-4 text-gray-600 rounded focus:ring-red-400 text-sm hover:text-red-600'
-                />
+                    <input
+                        type='checkbox'
+                        id='remember-me'
+                        name='remember-me'
+                        className='w-4 h-4 text-gray-600 rounded focus:ring-red-400 text-sm hover:text-red-600'
+                    />
 
-                <label htmlFor='remember-me' className='ml-2 block text-sm text-gray-600'>
-                    Remember me
-                </label>
+                    <label htmlFor='remember-me' className='ml-2 block text-sm text-gray-600'>
+                        Remember me
+                    </label>
 
-            </div>
+                </div>
 
-            <div className='text-sm'>
-                <a href='#' className='font-medium text-red-600 hover:text-red-400'>
-                    Forgot your password?
-                </a>
-            </div>
+                <div className='text-sm'>
+                    <a href='#' className='font-medium text-red-600 hover:text-red-400'>
+                        Forgot your password?
+                    </a>
+                </div>
 
-        </div >
-    )
+            </div >
+        )
 };
 
 export default FormExtra;

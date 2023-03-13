8a4e3c5331f2d8c50c415d1ed66ccd7a03622c95
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-03-13 17:20:38 -0600

    Add Sidebar, UserProfile, & Pins, and update Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { HiMenu, AiFillCloseCircle } from 'react-icons/ai';
+import { link, Route, Routes } from 'react-router-dom';
+
+import { Sidebar, UserProfile } from '../components';
+import pins from '../containers/Pins';
+import { client } from '../client';
+import logo from '../assets/me_share5.png';
 
 const Home = () => {
     return (

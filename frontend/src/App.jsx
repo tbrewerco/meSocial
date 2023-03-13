ab58dddd95cf3f7fb14298bd3462883b3ab0a039
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-03-13 17:21:22 -0600

    Update/remove dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,14 @@
 import { React, useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home.jsx';
 import LoginPage from './pages/loginPage.jsx';
-import SignupPage from './pages/signupPage.jsx';
 import './App.css';
 
 const App = () => {
 
-  // useEffect(() => {
-  //   function start() {
-  //     gapi.auth2.init({
-  //       clientId: import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID,
-  //       scope: ''
-  //     });
-  //   };
-  //   gapi.load('client:auth2', start);
-  // });
-
   return (
     <Routes>
       <Route path="login" element={<LoginPage />} />
-      <Route path="signup" element={<SignupPage />} />
       <Route path="/*" element={<Home />} />
     </Routes>
   );

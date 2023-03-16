ea2b7cda8386f13bd8e745e7d43a6f9cc052b76d
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-03-16 15:31:45 -0600

    WIP: Import dependencies

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
+
+import logo from '../assets/me_share5.png';
 
 const Sidebar = () => {
     return (

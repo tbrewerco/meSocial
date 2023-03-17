f4784e156ed7d86e2d5310c5994307e348c5a55c
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-03-17 12:54:54 -0600

    Update mobile sidebar slide in, add deps

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -74,7 +74,7 @@ module.exports = {
         },
       },
       animation: {
-        'slide-in': 'slide-in 0.5s ease-out',
+        'slide-in': 'slide-in 0.15s ease-out',
         'slide-fwd': ' slide-fwd 0.45s cubic-bezier(0.250, 0.460, 0.450, 0.940) both',
       },
       transitionProperty: {

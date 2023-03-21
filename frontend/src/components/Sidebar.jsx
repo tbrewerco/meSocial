c45d15a1da90f897c4d4bd393cd88d75e2b5d9d5
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-03-20 18:23:06 -0600

    Add to sidebar logo which is link that clsoes sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,9 +5,24 @@ import { IoIosArrowForward } from 'react-icons/io';
 
 import logo from '../assets/me_share5.png';
 
-const Sidebar = () => {
+const Sidebar = ({ user, closeToggle }) => {
+    const handleCloseSidebar = () => {
+        if (closeToggle) closeToggle(false)
+    }
+
     return (
-        <div>sidebar</div>
+        <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
+            <div className='flex flex-col'>
+                <Link
+                    to="/"
+                    className='flex px-f gap-2 my-6 pt-1 w-190 items-center'
+                    onClick={handleCloseSidebar}
+                >
+                    <img src={logo} alt='logo' className='w-full' />
+                </Link>
+            </div>
+            idebar
+        </div>
     )
 }
 

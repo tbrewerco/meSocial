cf0eacd10d796539f69a35990412aa2339985d47
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-04-28 17:59:39 -0600

    Add spinner

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { RevolvingDot } from 'react-loader-spinner';
+
+const Spinner = ({ message }) => {
+  return (
+    <div className='flex flex-col justify-center items-center w-full h-full'>
+      <RevolvingDot
+        width="300"
+        color="#cd1717"
+        className='m-5'
+      />
+
+      <p className='text-lg text-center px-2'>{message}</p>
+    </div>
+  )
+}
+
+export default Spinner

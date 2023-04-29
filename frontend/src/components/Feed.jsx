2256cda7e0dd38f407a13cf4665928dd7acf64d1
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-04-28 18:00:17 -0600

    Add fetch queries and masonrylayout

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const Feed = () => {
+  const [loading, setLoading] = useState(true);
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    setLoading(true)
+
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+
+      clearInterval.fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false)
+        })
+    } else {
+      client.fetch(feedQuery)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        })
+    }
+  }, [categoryId]);
+
+
+  if (loading) return <Spinner message='Working on it' />
+
   return (
-    <div>Feed</div>
+    <div>
+      {pins && <MasonryLayout pins={pins} />}
+    </div>
   )
 }
 

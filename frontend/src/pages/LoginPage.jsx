a1778ca18600e0298de709077b8dc60bc182ada3
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-05-17 19:45:01 -0600

    Adds scrollbar

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -18,7 +18,6 @@ const LoginPage = () => {
         <div>
             <div className='flex justify-start items-center flex-col h-screen'>
                 <div className='relative w-full h-full'>
-
                     <video
                         src={danceVid}
                         type="video/mp4"
@@ -31,8 +30,7 @@ const LoginPage = () => {
 
                 </div>
             </div>
-            <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
-                {/* to-do: responsive resising */}
+            <div className='absolute flex flex-col overflow-scroll justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
                 <div className="bg-white min-h-332px rounded-lg flex items-center justify-center py-12 px-12" >
                     <div >
                         <Header

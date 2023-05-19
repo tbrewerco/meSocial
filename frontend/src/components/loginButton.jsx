ac37adab07e452e684b914f5d53afbb47496cf4e
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-05-19 14:12:17 -0600

    Updates button to use express backend instead of old sanity backend

--- frontend/src/components/loginButton.jsx
+++ frontend/src/components/loginButton.jsx
@@ -6,36 +6,27 @@ import { client } from '../client';
 const LoginButton = () => {
     const navigate = useNavigate();
 
+    const URL = import.meta.env.VITE_SANITY_PROJECT_GOOGLE_AUTH_URL;
+
     const success = async (response) => {
-        localStorage.setItem('user', JSON.stringify(jwt_decode(response.credential)));
+        const id_token = response.credential
 
-        const { email, name, jti, picture } = jwt_decode(response.credential);
+        const serverResponse = await fetch(URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id_token })
+        });
 
-        // form and save new user document to sanity
-        const doc = {
-            _id: jti,
-            _type: 'user',
-            username: name,
-            image: picture,
-            email: email
+        if (!serverResponse.ok) {
+            throw new Error('Failed to authenticate with server.');
         };
 
-        try {
-            // check if user is already in the database
-            const query = `count(*[_type == 'user' && email == '${email}'])`;
-            const existingUser = await client.fetch(query);
+        const { session } = await serverResponse.json();
+        localStorage.setItem('session', session);
 
-            // if (existingUser) {
-            // throw new Error('Email is already in use')
-            // } else {
-            await client.createIfNotExists(doc)
-            navigate('/', { replace: true });
-            // }
-
-        } catch (error) {
-            console.error(`Error creating user document: ${error.message}`);
-        }
-
+        navigate('/', { replace: true });
     };
 
     const failure = (response) => {
@@ -43,9 +34,7 @@ const LoginButton = () => {
     };
 
     return (
-
         <div className='g_id_signin flex w-full justify-center items-center p-1 cursor-pointer rounded-md outline-1 outline-white outline hover:outline-red-300'>
-
             <GoogleLogin
                 shape='rectangular'
                 width='300'
@@ -56,7 +45,6 @@ const LoginButton = () => {
                 onFailure={failure}
                 cookiePolicy='single_host_origin'
             />
-
         </div>
     );
 };

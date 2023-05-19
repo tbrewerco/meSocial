1c33be6d9ff5445d28febd1465a00f3cdff98362
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-05-18 21:48:55 -0600

    Adds google auth route

--- backend/express/app.js
+++ backend/express/app.js
@@ -4,12 +4,16 @@ import cors from 'cors';
 import morgan from 'morgan';
 import sequelize from './index.js';
 
+import authRoutes from './routes/authRoutes.js';
+
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.json({ message: "meSocial v1" });

089e20d6516f32e872c789bc7039b37733142f31
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-05-19 10:53:21 -0600

    Moves dotenv config to dbConfig

--- backend/express/config/dbConfig.js
+++ backend/express/config/dbConfig.js
@@ -1,3 +1,13 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 export default {
     development: {
         username: process.env.DB_DEV_USERNAME,

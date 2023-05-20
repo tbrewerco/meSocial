d7353ff99b332acadcd6f0b072f1b11aa8c87929
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-05-19 18:00:15 -0600

    Adds validation, password validation, password hooks, and password validator function

--- backend/express/models/user.js
+++ backend/express/models/user.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs';
+
 export default (sequelize, DataTypes) => {
     const User = sequelize.define('user', {
         name: {
@@ -7,7 +9,10 @@ export default (sequelize, DataTypes) => {
         username: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                isAlphanumeric: true
+            }
         },
         image: {
             type: DataTypes.STRING,
@@ -20,31 +25,53 @@ export default (sequelize, DataTypes) => {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                isEmail: true
+            }
         },
-        googleId: {
+        google_id: {
             type: DataTypes.STRING(512),
             allowNull: true,
-        }
+        },
+        hooks: {
+            beforeCreate: async (user) => {
+                const salt = await hasBrowserCrypto.genSaltSnc(10, a);
+                user.password = bcrypt.hashSync(user.password, salt);
+            },
+            beforeUpdate: async (user) => {
+                if (user.changed('password')) {
+                    const salt = await bcrypt.genSaltSnc(user.password, salt);
+                    user.password = bcrypt.hashSync(user.password, salt);
+                }
+            }
+        },
+
     });
 
-    User.findOrCreateFromGoogleId = async function ({ sub, email, name, picture }) {
-        try {
-            const [user] = await User.findOrCreate({
-                where: { googleId: sub },
-                defaults: {
-                    name: name,
-                    email: email,
-                    username: email,
-                    image: picture,
-                    googleId: sub
-                }
-            });
-            return user;
-        } catch (error) {
-            console.error('Error during findOrCreateFromGoogleId:', error);
-            throw error;
-        };
-    };
     return User;
 };
+
+User.prototype.validPassword = async (password) => {
+    return await bcrypt.compare(password, this.password);
+};
+
+User.findOrCreateFromGoogleId = async ({ sub, email, name, picture }) => {
+    try {
+        const [user] = await User.findOrCreate({
+            where: { googleId: sub },
+            defaults: {
+                name: name,
+                email: email,
+                username: email,
+                image: picture,
+                googleId: sub
+            }
+        });
+        return user;
+    } catch (error) {
+        console.error('Error during findOrCreateFromGoogleId:', error);
+        throw error;
+    };
+};
+

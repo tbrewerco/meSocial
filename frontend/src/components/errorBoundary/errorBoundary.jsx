d6520616aae300d89f1510c2f69795828353fecf
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-06-03 18:41:37 -0600

    Adds comments to ErrorBoundary

--- frontend/src/components/errorBoundary/errorBoundary.jsx
+++ frontend/src/components/errorBoundary/errorBoundary.jsx
@@ -3,14 +3,19 @@ import React from 'react';
 class ErrorBoundary extends React.Component {
     constructor(props) {
         super(props);
+
+        // keep track of error
         this.state = { hasError: false, errorMessage: '' };
     }
 
+    // update component state when error is encountered
     static getDerivedStateFromError(error) {
         return { hasError: true, errorMessage: error.message };
     }
 
+    // log error when error is caught
     componentDidCatch(error, errorInfo) {
+        console.log("There's been an error caught by ErrorBoundary")
         console.error(error, errorInfo);
     }
 

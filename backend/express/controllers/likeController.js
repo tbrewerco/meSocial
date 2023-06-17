7889e2149923f8c54afc0e43b8b29c2673946af8
Travis Brewer <78047938+tbrewerco@users.noreply.github.com>
2023-06-16 19:17:45 -0600

    Adds checkLike and createLike to like controller

---
+++ backend/express/controllers/likeController.js
@@ -0,0 +1,29 @@
+import models from '../models/index.js';
+
+const Like = models.Like;
+
+const checkLike = async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        const pinId = req.params.pinId;
+        const like = await Like.checkLike(userId, pinId);
+        res.status(200).send(!!like)
+    } catch (error) {
+        console.error(error);
+        next(error);
+    };
+};
+
+const createLike = async (req, res, next) => {
+    try {
+        const userId = req.body.userId;
+        const pinId = req.body.pinId;
+        const like = await Like.createLike(userId, pinId);
+        res.status(200).send(like);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    };
+};
+
+export default { checkLike, createLike };
